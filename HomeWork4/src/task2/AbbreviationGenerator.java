2a0600fe0a4a880fd0a37710f79923561989eac0
Retro <max40a@mail.ru>
2016-11-13 15:16:51 +0200

    Fix flaws #2

--- HomeWork4/src/task2/AbbreviationGenerator.java
+++ HomeWork4/src/task2/AbbreviationGenerator.java
@@ -2,14 +2,16 @@ package task2;
 
 public class AbbreviationGenerator {
 
+    private static final int MAX_WORD_LENGTH = 10;
+
     public static String squeezeWord(String word) {
-        if (word.length() <= 10) {
+        if (word.length() <= MAX_WORD_LENGTH) {
             return word;
         }
 
         StringBuilder abbr = new StringBuilder();
         abbr.append(word.charAt(0));
-        abbr.append(String.valueOf(word.length() - 2));
+        abbr.append(word.length()-2);
         abbr.append(word.charAt(word.length() - 1));
 
         return abbr.toString();
